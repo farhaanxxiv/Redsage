06fcdc29e14b6ca45150b90eff94b33f3e27751f
farhaanxxiv <70432375+farhaanxxiv@users.noreply.github.com>
2025-01-08 17:15:46 +0530

    WIP: add lazy loading to home page images, fix ui

--- home.js
+++ home.js
@@ -58,58 +58,46 @@ buttons.forEach((button) => {
 
 
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-const timelineBar = document.querySelector(".placed .timeline .bar");
+// const timelineBar = document.querySelector(".placed .timeline .bar");
 
-// Desktop animation: left to right
-gsap.fromTo(
-  timelineBar,
-  { scaleX: 0, transformOrigin: "left center" },
-  {
-    scaleX: 1,
-    ease: "linear",
-    scrollTrigger: {
-      trigger: ".placed",
-      start: "top 80%",
-      end: "bottom 50%",
-      scrub: true,
-    },
-  }
-);
+// // Desktop animation: left to right
+// gsap.fromTo(
+//   timelineBar,
+//   { scaleX: 0, transformOrigin: "left center" },
+//   {
+//     scaleX: 1,
+//     ease: "linear",
+//     scrollTrigger: {
+//       trigger: ".placed",
+//       start: "top 80%",
+//       end: "bottom 50%",
+//       scrub: true,
+//     },
+//   }
+// );
 
-// Mobile animation: top to bottom
-ScrollTrigger.matchMedia({
-  "(max-width: 900px)": () => {
-    gsap.fromTo(
-      timelineBar,
-      { scaleY: 0, transformOrigin: "top center" },
-      {
-        scaleY: 1,
-        ease: "linear",
-        scrollTrigger: {
-          trigger: ".placed",
-          start: "top 80%",
-          end: "bottom 20%",
-          scrub: true,
-        },
-      }
-    );
-  },
-});
-
-
-// Select all buttons inside the student cards
-// const buttons = document.querySelectorAll('.testimonial-container button');
-
-// // Loop through each button and add an event listener
-// buttons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     // Toggle the 'active' class on the parent card
-//     const card = button.closest('.testimonial-container');
-//     card.classList.toggle('active');
-//   });
+// // Mobile animation: top to bottom
+// ScrollTrigger.matchMedia({
+//   "(max-width: 900px)": () => {
+//     gsap.fromTo(
+//       timelineBar,
+//       { scaleY: 0, transformOrigin: "top center" },
+//       {
+//         scaleY: 1,
+//         ease: "linear",
+//         scrollTrigger: {
+//           trigger: ".placed",
+//           start: "top 80%",
+//           end: "bottom 20%",
+//           scrub: true,
+//         },
+//       }
+//     );
+//   },
 // });
+
 
 
 const splide = new Splide('.students-testimonials-splide', {
@@ -127,17 +115,6 @@ const heroSplide = new Splide('.hero-splide', {
   arrows: false, // Enable navigation arrows,
   drag:false
 }).mount();
-
-// const container = document.querySelector('.testimonial-container');
-
-// const resizeObserver = new ResizeObserver(() => {
-//   splide.options = { width: container.offsetWidth }; // Update options dynamically
-//   splide.update(); // Refresh Splide layout
-// });
-
-// // Start observing
-// resizeObserver.observe(container);
-
 
 
 const heroCourseButtons = document.querySelectorAll('.hero-course-btn');
